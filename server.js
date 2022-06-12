b161ec345aa2b3c68a8c4dbc8240583fd101a50c
Carlos <carlosdaniel.londono@utp.edu.co>
2022-06-12 14:27:46 -0500

    JWT authentication

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const Hapi = require('@hapi/hapi')
 const mongoosePlugin = require('./src/plugins/mongoose')
+const checkAuthenticationPlugin = require('./src/plugins/check-authentication')
 const routes = require('./src/routes/index')
 require('dotenv').config()
 
@@ -10,6 +11,7 @@ const init = async () => {
   })
 
   await server.register(mongoosePlugin)
+  await server.register(checkAuthenticationPlugin)
   server.route(routes)
   server.start()
   console.log('Server running on %s', server.info.uri)
